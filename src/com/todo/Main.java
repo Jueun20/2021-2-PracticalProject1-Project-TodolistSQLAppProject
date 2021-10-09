608835184c35e3d40cab2e5528b0b9412fdca70e
seojueun <seojueun@localhost>
2021-10-09 17:45:57 +0900

    add SQLException in Main.java

--- src/com/todo/Main.java
+++ src/com/todo/Main.java
@@ -1,10 +1,11 @@
 package com.todo;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLException {
 		
 		TodoMain.start();
 		
